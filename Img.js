b996853269909bc83b47503533b6baf45dfaf85f
srikanth14072002 <137384368+srikanth14072002@users.noreply.github.com>
2023-06-23 16:26:36 +0530

    Update Img.js

--- Img.js
+++ Img.js
@@ -2,7 +2,7 @@ var allimage=document.images;
 console.log(allimage)
 for(i=0;i<allimage.length;i++)
 {
-    allimage[i].src=`D:/WEB TECHONOLOGY/JAVASCRIPT/Gallery/photos/${i}.jpg`
+    allimage[i].src=`D:/WEB TECHONOLOGY/JAVASCRIPT/image/photos/${i}.jpg`
     allimage[i].addEventListener("click",function()
     {
         document.getElementById("yoo").style.backgroundImage=`url(${this.src})`
