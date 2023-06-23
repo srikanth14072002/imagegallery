ea57b7ea95ddecf6f46a40c2fbb685265c2062bc
srikanth14072002 <137384368+srikanth14072002@users.noreply.github.com>
2023-06-23 15:46:46 +0530

    Add files via upload

---
+++ Img.js
@@ -0,0 +1,10 @@
+var allimage=document.images;
+console.log(allimage)
+for(i=0;i<allimage.length;i++)
+{
+    allimage[i].src=`D:/WEB TECHONOLOGY/JAVASCRIPT/Gallery/photos/${i}.jpg`
+    allimage[i].addEventListener("click",function()
+    {
+        document.getElementById("yoo").style.backgroundImage=`url(${this.src})`
+    })
+}
